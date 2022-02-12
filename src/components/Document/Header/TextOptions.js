2b0bda2d352868e8ebf46fd08fdaab4786022cb6
rxagar10 <rxagar10@gmail.com>
2022-02-12 01:40:22 -0500

    Header

--- src/components/Document/Header/TextOptions.js
+++ src/components/Document/Header/TextOptions.js
@@ -1,5 +1,6 @@
 import './TextOptions.scss';
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
+
 
 /* <a href="https://icons8.com/icon/98153/align-left">Align Left icon by Icons8</a>
 "https://img.icons8.com/external-tal-revivo-bold-tal-revivo/12/000000/external-bullet-list-template-option-in-word-document-application-text-bold-tal-revivo.png"
@@ -9,54 +10,25 @@ import React, {useState} from 'react';
 
 export default function TextOptions(props) {
 
-    /* When the user clicks on the button, 
-    toggle between hiding and showing the dropdown content */
-    const myFunction = () => {
-        document.getElementById("myDropdown").classList.toggle("show");
+    const changeFont = () => {
+        
     }
-  
-    // Close the dropdown menu if the user clicks outside of it
-    window.onclick = function(event) {
-        if (!event.target.matches('.dropbtn')) {
-            var dropdowns = document.getElementsByClassName("dropdown-content");
-            var i;
-            for (i = 0; i < dropdowns.length; i++) {
-                var openDropdown = dropdowns[i];
-                if (openDropdown.classList.contains('show')) {
-                    openDropdown.classList.remove('show');
-                }
-            }
-        }
-    }
-
-
 
     return (
         <div className='textOptions'>
             <button onClick={() => props.setTextStyles({ ...props.textStyles, bold: !props.textStyles.bold })}> <b>B</b> </button>
             <button onClick={() => props.setTextStyles({ ...props.textStyles, italicize: !props.textStyles.italicize })}> <i>I</i> </button>
             <button onClick={() => props.setTextStyles({ ...props.textStyles, underline: !props.textStyles.underline })}> <u>U</u> </button>
-            <div class="dropdown">
-                <button onClick={() => myFunction()} class='dropbtn'>
-                    {props.textStyles.font}
-                </button>
-                <div id="myDropdown" class="dropdown-content">
-                    <a href="#">Link 1</a>
-                    <a href="#">Link 2</a>
-                    <a href="#">Link 3</a>
-                </div>
-            </div>
+            <select className='dropDown' onchange='changeFont()'>
+                <option value="Calibri">Calibri</option>
+                <option value="Times New Roman">Times New Roman</option>
+                <option value="Arial">Arial</option>
+            </select>
+            
+
+
             <input type="text"/>
-            <div class="dropdown">
-                <button onClick={() => myFunction()} class='dropbtn'>
-                    {props.textStyles.fontColor}
-                </button>
-                <div id="myDropdown" class="dropdown-content">
-                    <a href="#">Link 1</a>
-                    <a href="#">Link 2</a>
-                    <a href="#">Link 3</a>
-                </div>
-            </div>
+            
             <button> <img src="https://img.icons8.com/external-tal-revivo-bold-tal-revivo/24/000000/external-bullet-list-template-option-in-word-document-application-text-bold-tal-revivo.png"/>
             </button> 
             <button> <img src="https://img.icons8.com/external-tal-revivo-bold-tal-revivo/24/000000/external-number-list-format-in-acending-sequence-order-text-bold-tal-revivo.png"/>
@@ -70,9 +42,3 @@ export default function TextOptions(props) {
         </div>
     )
 }
-
-/*
-setTextStyles({
-    ...textStyles,
-    bold: true
-}) */
