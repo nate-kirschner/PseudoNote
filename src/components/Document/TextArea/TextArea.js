ddd7042019055aac11b11893de69f0537f975ed8
nate-kirschner <77256794+nate-kirschner@users.noreply.github.com>
2022-02-12 00:04:00 -0500

    text area implementation

--- src/components/Document/TextArea/TextArea.js
+++ src/components/Document/TextArea/TextArea.js
@@ -1,9 +1,40 @@
+import React, { useEffect, useState, useRef } from 'react';
+import TextField from './TextField/TextField';
+
 import './TextArea.scss';
 
 export default function TextArea() {
+
+    const [textBoxes, setTextBoxes] = useState([]);
+    const [textBoxPositions, setTextBoxPositions] = useState([]);
+
+    const handleTextAreaClicked = (e) => {
+        const top = e.clientY;
+        const left = e.clientX;
+
+        let outsideTextBox = true;
+        textBoxPositions.forEach(box => {
+            outsideTextBox = 
+                outsideTextBox && 
+                ((top < box.top || top > (box.top + box.height)) ||
+                (left < box.left || left > (box.left + box.width)))
+        })
+        if (outsideTextBox) {
+            setTextBoxPositions([...textBoxPositions, {top: top, left: left, width: 250, height: 300}])
+            setTextBoxes([
+                ...textBoxes, 
+                <TextField top={top} left={left} />
+            ])
+        }
+    }
+
     return (
-        <div className="textArea">
-            Text Area
+        <div className="textArea"
+            onClick={(e) => handleTextAreaClicked(e)}
+        >
+            {
+                textBoxes
+            }
         </div>
     )
 }
